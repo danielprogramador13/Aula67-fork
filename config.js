2c918d3520b7a32daf3f3e6002ac634fb49b1c80
danielprogramador13 <danielprogramadorcosta@gmail.com>
2022-12-03 10:41:39 -0300

    Database

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+   // Your web app's Firebase configuration
+  apiKey: "AIzaSyBDkPC9UVlylaUNkvWB5w4sZqt6P0WJU84",
+  authDomain: "aula67-fork.firebaseapp.com",
+  projectId: "aula67-fork",
+  storageBucket: "aula67-fork.appspot.com",
+  messagingSenderId: "730963386265",
+  appId: "1:730963386265:web:a7d128577ce460deaf7d08"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
